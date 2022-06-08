b5b5c28398ad9528061e2466a6b1c4561cca95e6
Julian <perassijulian@gmail.com>
2022-06-08 19:25:50 -0300

    raffle smart contract

---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Raffle = await hre.ethers.getContractFactory("Raffle");
+  const raffle = await Raffle.deploy();
+  await raffle.deployed();
+
+  console.log("Raffle deployed to: ", raffle.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
