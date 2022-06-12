bc2fbe9b240955d7509cebb3dfee78836be36934
Julian <perassijulian@gmail.com>
2022-06-12 18:19:38 -0300

    styling with tailwind

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,7 +2,8 @@ import { ConnectButton } from "web3uikit";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between border-b-2 p-2 w-full">
+      <h1 className="text-xl font-bold">WEB3 LOTTERY</h1>
       <ConnectButton moralisAuth={false}/>
     </div>
   );
--- client/components/RaffleEntrance.jsx
+++ client/components/RaffleEntrance.jsx
@@ -3,13 +3,17 @@ import { useEffect, useState } from "react";
 import { useMoralis, useWeb3Contract } from "react-moralis";
 import abi from "../constants/abi";
 import contractAddresses from "../constants/contractAddresses";
+import { useNotification } from "web3uikit";
 
 const RaffleEntrance = () => {
+  const [entranceFee, setEntranceFee] = useState("0");
+  const [numPlayers, setNumPlayers] = useState("0");
+  const [lastWinner, setLastWinner] = useState(null);
+  const dispatch = useNotification();
   const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
   const chainId = parseInt(chainIdHex);
   const raffleAddress =
     chainId in contractAddresses ? contractAddresses[chainId][0] : null;
-  const [entranceFee, setEntranceFee] = useState("0");
 
   const { runContractFunction: enterRaffle } = useWeb3Contract({
     abi: abi,
@@ -26,27 +30,69 @@ const RaffleEntrance = () => {
     params: {},
   });
 
-  const handleEnter = async () => {
-    const res = await enterRaffle()
-    console.log('res:', res);
-  }
+  const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getNumberOfPlayers",
+    params: {},
+  });
+
+  const { runContractFunction: getRecentWinner } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getRecentWinner",
+    params: {},
+  });
+
+  const handleSuccess = async (tx) => {
+    await tx.wait(1);
+    handleNotification();
+    updateUI();
+  };
+
+  const handleNotification = () => {
+    dispatch({
+      type: "info",
+      message: "You are in the raffle!",
+      title: "Transaction succeded",
+      icon: "bell",
+      position: "topR",
+    });
+  };
+
+  const updateUI = async () => {
+    const entranceFeeCall = (await getEntranceFee()).toString();
+    const lastWinnerCall = await getRecentWinner();
+    const numPlayersCall = (await getNumberOfPlayers()).toString();
+    setEntranceFee(entranceFeeCall);
+    setLastWinner(lastWinnerCall);
+    setNumPlayers(numPlayersCall);
+  };
 
   useEffect(() => {
     if (isWeb3Enabled) {
-      const updateUI = async () => {
-        const entranceFeeCall = (await getEntranceFee()).toString();
-        setEntranceFee(entranceFeeCall);
-      };
       updateUI();
     }
   }, [isWeb3Enabled]);
 
   return (
-    <div>
+    <div className="flex justify-center p-3">
       {raffleAddress ? (
-        <div>
-          Entrance fee: {ethers.utils.formatUnits(entranceFee, "ether")}
-          <button onClick={handleEnter}>Enter raffle</button>
+        <div className="flex flex-col items-center">
+          <span>Entrance fee: {ethers.utils.formatUnits(entranceFee, "ether")}</span>
+          <span>Number of players: {numPlayers}</span>
+          <span>Last winner: {lastWinner}</span>
+          <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4 mt-2"
+            onClick={async () =>
+              await enterRaffle({
+                onSuccess: handleSuccess,
+                onError: (e) => console.log(e),
+              })
+            }
+          >
+            Enter raffle
+          </button>
         </div>
       ) : (
         <div>
