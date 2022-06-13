47ceb811f81df957204d1ba49d04e4f94473220d
Julian <perassijulian@gmail.com>
2022-06-13 11:55:40 -0300

    rearrange folders for deploy

--- blockchain/helper-hardhat-config.js
+++ blockchain/helper-hardhat-config.js
@@ -30,8 +30,8 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"];
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
-const frontEndContractsFile = "./client/constants/contractAddresses.json";
-const frontEndAbiFile = "./client/constants/abi.json";
+const frontEndContractsFile = "../constants/contractAddresses.json";
+const frontEndAbiFile = "../constants/abi.json";
 
 module.exports = {
   networkConfig,
